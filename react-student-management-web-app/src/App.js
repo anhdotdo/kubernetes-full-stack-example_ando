13c63d580b66a77308f61d2396006d95a9e36461
anhdotdo <anhdo157hcl@gmail.com>
2022-07-25 10:37:00 +0000

    Modify App.js

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,9 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-
             Student management Application - dsqlsv
-
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
